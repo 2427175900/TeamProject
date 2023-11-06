0c0371b9a51bab571c44628963b291d23f1adf92
yuanfulong <yuanfulong326@gmail.com>
2023-11-06 14:23:44 +0900

    添加了图片转换效果窗口和降噪按钮
    1

--- TeamProject/resource.h
+++ TeamProject/resource.h
@@ -1,21 +1,31 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++에서 생성한 포함 파일입니다.
-// TeamProject.rc에서 사용되고 있습니다.
+// Microsoft Visual C++ 生成的包含文件。
+// 供 TeamProject.rc 使用
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
 #define IDD_TEAMPROJECT_DIALOG          102
 #define IDR_MAINFRAME                   128
+#define IDD_OUTIMG_DLG                  130
 #define IDC_BUTTON1                     1000
+#define IDC_autogroup                   1001
+#define IDC_manualgroup                 1002
+#define IDC_BUTTONauto                  1003
+#define IDC_BUTTON3                     1004
+#define IDC_BUTTON4                     1005
+#define IDC_BUTTON5                     1006
+#define IDC_BUTTON6                     1007
+#define IDC_CHECK1                      1008
+#define IDC_OUTBUT                      1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
